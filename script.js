435c2db21c0361194dca006f6d7a836b417c2635
boydy44 <78049043+boydy44@users.noreply.github.com>
2023-09-25 20:21:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // Pop up menu
 
 let openMenuButton = document.querySelector("#openMenuButton");
+let closeMenuButton = document.querySelector("#closeMenuButton");
 let popUpMenu = document.querySelector("#popUpMenu");
 
 openMenuButton.addEventListener("click", openMenu);
+closeMenuButton.addEventListener("click", closeMenu);
 
 function openMenu() {
   popUpMenu.classList.add("pop-up-menu-container-open");
+}
+
+function closeMenu() {
+  popUpMenu.classList.remove("pop-up-menu-container-open");
 }
 
 // Calculate number of days until wedding
